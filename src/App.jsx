43275ba2bf5d934679376bbbe4f096f2a09cae2f
Andre Barber <andrepbarber@gmail.com>
2026-02-19 03:34:32 -0800

    Add Signup route and improve route comments
    Added Signup route and updated root route comments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // menubloc-frontend/src/App.jsx
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -7,23 +6,31 @@ import EasyMenuLanding from "./pages/EasyMenuLanding";
 import GrubbidHome from "./pages/GrubbidHome";
 import GrubbidSearch from "./pages/GrubbidSearch";
 import RestaurantProfile from "./pages/RestaurantProfile";
+import Signup from "./pages/Signup";
 
 export default function App() {
   const hostname = window.location.hostname;
 
   const isEasyMenu =
-    hostname === "easymenuupload.com" ||
-    hostname === "www.easymenuupload.com";
+    hostname === "easymenuupload.com" || hostname === "www.easymenuupload.com";
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={isEasyMenu ? <EasyMenuLanding /> : <GrubbidHome />}
-        />
+        {/* Root route depends on domain */}
+        <Route path="/" element={isEasyMenu ? <EasyMenuLanding /> : <GrubbidHome />} />
+
+        {/* Keep the rest of Grubbid routes available on both domains */}
         <Route path="/search" element={<GrubbidSearch />} />
+
+        {/* Restaurant profile route (use whichever your app expects) */}
         <Route path="/restaurant/:id" element={<RestaurantProfile />} />
+
+        {/* If you still use signup */}
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Optional: simple fallback */}
+        <Route path="*" element={isEasyMenu ? <EasyMenuLanding /> : <GrubbidHome />} />
       </Routes>
     </BrowserRouter>
   );
