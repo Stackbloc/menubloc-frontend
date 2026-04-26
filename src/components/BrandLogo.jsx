3ebf8b2bd4de91a364592cc09873a1170f6e97a8
Andre Barber <helloandrebarber@gmail.com>
2026-04-26 03:03:33 -0700

    Replace shared logo with transparent Menuply asset

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -12,7 +12,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const GRUBBID_LOGO_SRC = "/grubbid-logo-2.1.svg";
+export const GRUBBID_LOGO_SRC = "/menuply-logo5-transparent.png";
 
 export function BrandLogo({
   to = "/",
